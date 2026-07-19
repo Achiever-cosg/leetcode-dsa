60344111455d70f2bdcc1313eca9aef67d37a26f
Samshul Ajam <93369157+Achiever-cosg@users.noreply.github.com>
2026-07-19 23:16:51 +0530

    [12] [Time Beats: 76.27%] [Memory Beats: 68.50%] - Leet2Hub

--- 01-Arrays-and-Hashing/12. Integer to Roman.cpp
+++ 01-Arrays-and-Hashing/12. Integer to Roman.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
 string intToRoman(int num) {
-string res = ;
-vector<string> sym = {M, CM, D, CD, C, XC, L, XL, X, IX, V, IV,  I};
+string res = "";
+vector<string> sym = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV",  "I"};
 vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 for(int i=0; i<val.size() && num != 0; i++)
 {
